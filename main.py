c8057abf5b7ae2b73227e129a5275794bc6d4871
Jason <jason.huang.2@stonybrook.edu>
2021-01-28 18:24:19 -0500

    Converting file complete

---
+++ main.py
@@ -0,0 +1,20 @@
+from networkx.readwrite import json_graph
+import networkx as nx
+
+import json
+import os, fnmatch
+if __name__ == "__main__":
+    listOfFiles = os.listdir('.')
+    pattern = "*.gml"
+    for entry in listOfFiles:
+        if fnmatch.fnmatch(entry, pattern):
+            graph_to_convert = nx.read_gml(entry)
+            data = json_graph.node_link_data(
+                graph_to_convert, {"link": "edges", "source": "from", "target": "to"}
+            )
+            with open(entry.split('.')[0] + '.json', 'w') as outfile:
+                json.dump(data, outfile, default={
+                    "link": "edges", "source": "from", "target": "to"
+                    })
+
+
